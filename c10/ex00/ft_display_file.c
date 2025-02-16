2e1507df114b66223ea7ac810c5e1433f420b4f8
NatimiDev <nati.mikula@gmail.com>
2025-02-16 15:00:57 +0100

    init commit ex01

---
+++ c10/ex00/ft_display_file.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_display_file.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmikuka <nmikuka@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/15 21:22:56 by nmikuka           #+#    #+#             */
+/*   Updated: 2025/02/16 14:59:17 by nmikuka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
+
+int	print_file_content(char *filename)
+{
+	int		fd;
+	int		buffer_size;
+	int		bytes_read;
+	char	content[1 << 6];
+
+	buffer_size = 1 << 6;
+	bytes_read = buffer_size;
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	while (bytes_read > 0)
+	{
+		bytes_read = read(fd, content, buffer_size - 1);
+		content[bytes_read] = '\0';
+		ft_putstr(content);
+	}
+	close(fd);
+	return (1);
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		ft_putstr("File name missing.");
+		return (1);
+	}
+	if (argc > 2)
+	{
+		ft_putstr("Too many arguments.");
+		return (1);
+	}
+	if (!print_file_content(argv[1]))
+	{
+		ft_putstr("Cannot read file.");
+		return (1);
+	}
+	return (0);
+}
