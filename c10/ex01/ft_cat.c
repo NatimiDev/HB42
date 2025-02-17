0edbf44cfe5d3d2bbc2d2be691f2b7e082fb4121
NatimiDev <nati.mikula@gmail.com>
2025-02-17 16:40:45 +0100

    rearrange functions to handle stdinput

--- c10/ex01/ft_cat.c
+++ c10/ex01/ft_cat.c
@@ -6,13 +6,12 @@
 /*   By: nmikuka <nmikuka@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 15:02:23 by nmikuka           #+#    #+#             */
-/*   Updated: 2025/02/16 15:31:08 by nmikuka          ###   ########.fr       */
+/*   Updated: 2025/02/16 16:16:34 by nmikuka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <fcntl.h>
-#include <errno.h>
 
 void	ft_putstr(char *str)
 {
@@ -23,57 +22,47 @@ void	ft_putstr(char *str)
 	}
 }
 
-int	print_file_content(char *filename)
+void	print_fd(int fd)
 {
-	int		fd;
 	int		buffer_size;
 	int		bytes_read;
 	char	content[1 << 6];
 
 	buffer_size = 1 << 6;
 	bytes_read = buffer_size;
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-		return (0);
 	while (bytes_read > 0)
 	{
 		bytes_read = read(fd, content, buffer_size - 1);
 		content[bytes_read] = '\0';
 		ft_putstr(content);
 	}
+}
+
+int	print_file_content(char *filename)
+{
+	int		fd;
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	print_fd(fd);
 	close(fd);
 	return (1);
 }
 
-void	print_stdin()
-{	
-	int		buffer_size;
-	int		bytes_read;
-	char	content[1 << 6];
-
-	buffer_size = 1 << 6;
-	bytes_read = buffer_size;
-	while (bytes_read > 0)
-	{
-		bytes_read = read(0, content, buffer_size - 1);
-		content[bytes_read] = '\0';
-		ft_putstr(content);
-	}
-}
-
 int	main(int argc, char *argv[])
 {
-	int i;
-    
+	int	i;
+
 	if (argc == 1)
 	{
-		print_stdin();
+		print_fd(0);
 		return (0);
 	}
 	i = 1;
 	while (i < argc)
 	{
-		if(!print_file_content(argv[i]))
+		if (!print_file_content(argv[i]))
 		{
 			ft_putstr("ft_cat: ");
 			ft_putstr(argv[i]);
